d46e8271130bbcec4fea56852dd3a057daf7198c
moraneko <moran974@hotmail.fr>
2020-10-25 18:19:42 +0100

    Page moniteur : Distribution des chevaux par cours fonctionnel mais reste a faire les cas où il n'y a pas assez d'user inscrit ou la prise compte du temps.

--- front/src/app/moniteurpage/moniteur-cours-modal/moniteur-cours-modal.component.ts
+++ front/src/app/moniteurpage/moniteur-cours-modal/moniteur-cours-modal.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MoniteurService } from '../moniteur.service';
+import { Combinaison } from '../../model/combinaison';
+import { Cheval } from '../../model/cheval';
 
 @Component({
   selector: 'app-moniteur-cours-modal',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoniteurCoursModalComponent implements OnInit {
 
-  constructor() { }
+  distributionUserCheval: Combinaison[];
+  listChevalDispo: Cheval[];
+  constructor(public dialogRef: MatDialogRef<MoniteurCoursModalComponent>,@Inject(MAT_DIALOG_DATA) public data: any, private moniteurService: MoniteurService) { }
 
   ngOnInit(): void {
+  console.log(this.data.cours.content);
+    this.moniteurService.getCombinaisonOfCours(this.data.cours.content.id_cours) // changer l'id ICI
+        .subscribe((data: Combinaison[]) => this.distributionUserCheval = data);
+    this.moniteurService.getChevalDispo(this.data.cours.content.id_cours) // changer l'id ICI
+        .subscribe((data: Cheval[]) => this.listChevalDispo = data);
+  }
+
+  public onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  chevalForUserSelected(combi, cheval){
+    this.moniteurService.attributionCheval(combi,cheval);
+    var index = this.listChevalDispo.indexOf(cheval);
+    this.listChevalDispo.splice(index,1);
+
   }
 
 }
--- front/src/app/moniteurpage/moniteur.service.ts
+++ front/src/app/moniteurpage/moniteur.service.ts
@@ -3,6 +3,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { Cours } from '../model/cours';
+import {Combinaison} from '../model/combinaison';
+import {Cheval} from '../model/cheval';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +15,39 @@ export class MoniteurService {
   constructor(private http: HttpClient) { }
   private getCoursUrl = '/cours/getMoniteur';
   private addCours = '/cours/newCours';
+  private getCombiUrl = '/cours/combiOfCours';
+  private getChevalDispoUrl = '/cheval/dispo';
+  private attrCheval = '/cheval/attr';
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
+  //Composant de test Temporaires.
   private coursMoniteur: Cours[] = [
-    {id_cours: 2, horaire: new Date("October 25, 2020 12:00:00"), taille_groupe: 10, niveau: 2 , recurrent: false, moniteur: 1, titre: ''},
+    {id_cours: 2, horaire: new Date("October 25, 2020 12:00:00"), taille_groupe: 10, niveau: 2 , recurrent: false, moniteur: 1, titre: 'Cours test'},
   ];
+
+  private distribution : Combinaison[] = [{
+    id_combi:1,
+    id_cours:2,
+    user: {id_user: 10, nom: 'Jean', prenom: 'Dupont', role: 0},
+    cheval: { id_cheval: 1, nom: 'El Condor Pasa', age: -1, sexe: '', couleur: '', taille: -1, race: ''}
+    },{
+    id_combi:2,
+    id_cours:2,
+    user: {id_user: 11, nom: 'Jean2', prenom: 'Dupont2', role: 0},
+    cheval: { id_cheval: -1, nom: '', age: -1, sexe: '', couleur: '', taille: -1, race: ''}
+    },{
+    id_combi:3,
+    id_cours:2,
+    user: {id_user: 12, nom: 'Jean3', prenom: 'Dupont3', role: 0},
+    cheval: { id_cheval: -1, nom: '', age: -1, sexe: '', couleur: '', taille: -1, race: ''}
+    }];
+
+  private listChevalDispo: Cheval[]= [
+    { id_cheval: 2, nom: 'Special Week', age: -1, sexe: '', couleur: '', taille: -1, race: ''},
+    { id_cheval: 3, nom: 'Golden Boat', age: -1, sexe: '', couleur: '', taille: -1, race: ''}
+    ];
 
 
   /*
@@ -41,5 +70,40 @@ export class MoniteurService {
       tap((result: Cours) => console.log(result)));
   }
 
+  /*
+  getCombinaisonOfCours(id_cours): Observable<Combinaison[]>{
+      const url = `${this.getCombiUrl}/${id_cours}`;
+      return this.http.get<Combinaison[]>(url).pipe(
+        tap((combi: Combinaison[]) => console.log(combi))
+        );
+  */
+  getCombinaisonOfCours(id_cours): Observable<Combinaison[]> { // a modifier quand back here
+    return of(this.distribution);
+  }
+
+  /*
+  getChevalDispo(id_cours): Observable<Cheval[]>{
+      const url = `${this.getChevalDispoUrl}/${id_cours}`;
+      return this.http.get<Cheval[]>(url).pipe(
+        tap((combi: Cheval[]) => console.log(combi))
+        );
+  */
+  getChevalDispo(id_cours): Observable<Cheval[]> { // a modifier quand back here
+    return of(this.listChevalDispo);
+  }
+
+  /*
+  attributionCheval(combinaison: Combinaison, cheval: Cheval) : Observable<Combinaison[]>{
+      const url = `${this.attrCheval}/${combinaison.id_cours}`;
+      return this.http.post<Combinaison[]>(url).pipe(
+        tap((combi: Combinaison[]) => console.log(combi))
+        );
+  */
+  attributionCheval(combinaison: Combinaison, cheval: Cheval) : Observable<Combinaison[]>  {
+    console.log(this.distribution.indexOf(combinaison));
+    var index = this.distribution.indexOf(combinaison);
+    this.distribution[index].cheval = cheval;
+    return of(this.distribution);
+  }
 
 }
